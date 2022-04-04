5cbf029cd9d1e4cd0ee760468ab971e8965aa3c3
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-04-04 19:16:49 +0530

    Add files via upload

---
+++ miss-number-array.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+  public:
+    int MissingNumber(vector<int>& array, int n) {
+         long long sum=n*(n+1)/2;
+        for(auto n: array)
+            sum-=n;
+        return sum;
+    }
+};
+
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin >> n; 
+        vector<int> array(n-1);
+        for (int i = 0; i <n-1; i++) cin>> array[i];
+        Solution obj;
+        cout<<obj.MissingNumber(array, n)<< "\n";
+        
+        
+    }
+
+
+return 0;
+}
