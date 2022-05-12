e1cbfe6898fbc2fd853b02e5a09648cb134e2d69
STREIN_11 <71601569+STREIN-11@users.noreply.github.com>
2022-05-13 00:01:57 +0530

    Update miss-number-array.cpp

--- miss-number-array.cpp
+++ miss-number-array.cpp
@@ -21,11 +21,8 @@ int main(){
         vector<int> array(n-1);
         for (int i = 0; i <n-1; i++) cin>> array[i];
         Solution obj;
-        cout<<obj.MissingNumber(array, n)<< "\n";
-        
-        
+        cout<<obj.MissingNumber(array, n)<< "\n";      
     }
 return 0;
-  
 //Driver Code Ends here }
 }
